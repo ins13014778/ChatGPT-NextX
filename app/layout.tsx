394d620d9ad5d371ac2cc1afe2d5c8dec4949f37
ins13014778 <133472503+ins13014778@users.noreply.github.com>
2023-09-11 23:19:44 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Nex",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT Pus",
+  description: "ChatGPT Pus.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Nex",
+    title: "ChatGPT Pus",
     statusBarStyle: "default",
   },
 };
